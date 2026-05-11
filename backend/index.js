a488cf998c33eb9b4f36161fa56cd525087be574
AKSHIT-IITB <177029944+AKSHIT-IITB@users.noreply.github.com>
2026-05-12 03:31:06 +0530

    connect it with Mongo Atlas

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
